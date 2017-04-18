75953125d6f24a709dce8b7150705bd82bc801a0
Daniel Clelland <daniel.clelland@gmail.com>
2017-04-18 17:53:19 +1200

    Fix everything for the new Integer protocol

--- Parity.swift
+++ Parity.swift
@@ -18,56 +18,23 @@ public enum Parity {
     case odd
 }
 
-public protocol IntegerParity {
-    
-    /// The number's parity: even or odd.
-    var parity: Parity { get }
-    
-}
-
 // MARK: - Helpers
 
-public extension IntegerParity {
-    
-    /// The number is even.
-    var isEven: Bool {
-        return parity == .even
-    }
-    
-    /// The number is odd.
-    var isOdd: Bool {
-        return parity == .odd
-    }
-    
-}
-
-// MARK: - Implementations
-
-extension IntegerType where Self: IntegerParity {
+extension Integer {
     
     /// The number's parity.
     public var parity: Parity {
         return self % 2 == 0 ? .even : .odd
     }
     
+    /// The number is even.
+    public var isEven: Bool {
+        return parity == .even
+    }
+    
+    /// The number is odd.
+    public var isOdd: Bool {
+        return parity == .odd
+    }
+    
 }
-
-extension Int: IntegerParity { }
-
-extension Int8: IntegerParity { }
-
-extension Int16: IntegerParity { }
-
-extension Int32: IntegerParity { }
-
-extension Int64: IntegerParity { }
-
-extension UInt: IntegerParity { }
-
-extension UInt8: IntegerParity { }
-
-extension UInt16: IntegerParity { }
-
-extension UInt32: IntegerParity { }
-
-extension UInt64: IntegerParity { }
--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Parity is an integer parity microlibrary. It tells you whether an integer is even or odd!
 
-Parity implements an `IntegerParity` protocol for all integer types (`Int`, `Int8`, `Int16`, `Int32`, `Int64`, `UInt`, `UInt8`, `UInt16`, `UInt32`, `UInt64`) and provides a default, generic implementation for future integer types as well (i.e. those that conform to `Swift.IntegerType`).
-
 ### Examples
 
 ✓ Helpers
